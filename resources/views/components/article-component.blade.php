a8893f137ec712615eecec9b7fa3137ab3e08e3a
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-11-25 08:09:01 +0630

    - improve voting ui

--- resources/views/components/article-component.blade.php
+++ resources/views/components/article-component.blade.php
@@ -5,32 +5,30 @@
                 @csrf
                 <input type="hidden" name="vote_type" value="{{$article->is_up_voted?0:1}}">
                 <input type="hidden" name="article_id" value="{{$article->id}}">
-                <button type="submit" class="btn"> <i class="fa fa-caret-up {{$article->is_up_voted?'text-primary':''}}"></i></button>
+                <button type="submit" class="btn p-0"> <i class="fa fa-caret-up {{$article->is_up_voted?'text-primary':''}}"></i></button>
             </form>
             <span>{{$article->vote_count}}</span>
             <form method="POST" action="{{route('votes.store')}}">
                 @csrf
                 <input type="hidden" name="vote_type" value="{{$article->is_down_voted?-2:-1}}">
                 <input type="hidden" name="article_id" value="{{$article->id}}">
-                <button type="submit" class="btn"> <i class="fa fa-caret-down {{$article->is_down_voted?'text-primary':''}}"></i></button>
+                <button type="submit" class="btn p-0"> <i class="fa fa-caret-down {{$article->is_down_voted?'text-primary':''}}"></i></button>
             </form>
         </div>
-        <div class="col-11">
+        <div class="col-10 col-md-11 d-flex flex-column justify-content-center">
             <div class="d-flex justify-content-between">
-                <h3>
-                    <a href="{{ route('articles.show', $article->id) }}">
+                    <a class="font-weight-bold" href="{{ route('articles.show', $article->id) }}">
                         {{ $article->title }}
                     </a>
-                </h3>
                 <small class="">{{ $article->updated_at->diffForHumans() }}</small>
             </div>
-            <p>by <span class="text-info">{{ $article->user->name }}</span></p>
+            <span>by <span class="text-info">{{ $article->user->name }}</span></span>
             <div>
                 @if (count($article->tags) > 0)
-                    <p>Tags: @foreach ($article->tags as $tag)
+                    <span>Tags: @foreach ($article->tags as $tag)
                             <span class="badge badge-success">{{ $tag->name }}</span>
                         @endforeach
-                    </p>
+                    </span>
                 @endif
 
             </div>
